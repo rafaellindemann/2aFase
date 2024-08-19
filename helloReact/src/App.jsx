346c68118494ff6f3da2145795c3864cf85c718f
rafaellindemann <rafaelld@gmail.com>
2024-08-19 17:07:43 -0300

    Adicionado helloReact

---
+++ helloReact/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='titulo1'>Hello React JS fofinho</h1>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae unde deleniti tempora! Explicabo recusandae sunt consectetur at error voluptates quam aperiam. Asperiores, totam mollitia cumque quisquam eum explicabo at cupiditate.</p>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae unde deleniti tempora! Explicabo recusandae sunt consectetur at error voluptates quam aperiam. Asperiores, totam mollitia cumque quisquam eum explicabo at cupiditate.</p>
+      <p className='paragrafo'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae unde deleniti tempora! Explicabo recusandae sunt consectetur at error voluptates quam aperiam. Asperiores, totam mollitia cumque quisquam eum explicabo at cupiditate.</p>
+    </>
+  )
+}
+
+export default App
